4034d69317b26a0a6f68d441d486a7ece223b2c4
buihanh2001 <greenlemon6885@gmail.com>
2025-06-20 19:18:47 +0700

    thêm x trở về

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://small-tires-glow.loca.lt/api";
+const API_BASE_URL = "https://early-geese-admire.loca.lt/api";
 function formatDate(date) {
   const d = new Date(date);
   const day = String(d.getDate()).padStart(2, "0");
